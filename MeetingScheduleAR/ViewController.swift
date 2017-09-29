d60e272d0727e12ca43524c91dad6a2c635b8758
ernie.cheng <ernie.cheng@perficient.com>
2017-09-29 14:29:32 +0800

    add a blank node when the user touch the ar camera coordinate

--- MeetingScheduleAR/ViewController.swift
+++ MeetingScheduleAR/ViewController.swift
@@ -13,7 +13,9 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+    var currentCameraCoordinates = myCameraCoordinates(x: 0,
+                                                       y: 0,
+                                                       z: 0)
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,24 +35,46 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
         // Run the view's session
         sceneView.session.run(configuration)
         addObject()
     }
-    
     
     func addObject() {
         let calendar = ARCalendar()
         calendar.loadModal()
         let appearPostion = SCNVector3Make(1, 1, 1)
         calendar.position = appearPostion
-        
         // add the calendar to the scene
         sceneView.scene.rootNode.addChildNode(calendar)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: sceneView)
+            //let hitList = sceneView.hitTest(location, types: nil)
+            // from the sceneARKit
+            let hitList = sceneView.hitTest(location, options: nil)
+            if let hitObject = hitList.first {
+               let node = hitObject.node
+                if node.name == "calendarTitle" {
+                    node.removeFromParentNode()
+                    addCube()
+                    print("label get touched")
+                }
+            }
+        }
+    }
+    
+    func addCube() {
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0))
+        cubeNode.position = SCNVector3Make(currentCameraCoordinates.x,
+                                           currentCameraCoordinates.y,
+                                           currentCameraCoordinates.z)
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -71,6 +95,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 */
     
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // Do something with the new transform
+        let currentTransform = frame.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: currentTransform)
+        currentCameraCoordinates = myCameraCoordinates(x: cameraCoordinates.translation.x,
+                                                       y: cameraCoordinates.translation.y,
+                                                       z: cameraCoordinates.translation.z)
+    }
+    
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -85,4 +118,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+}
+
+struct myCameraCoordinates {
+    var x = Float()
+    var y = Float()
+    var z = Float()
 }
