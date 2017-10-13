62cfdf5b11476485ba43de862e869cd985efac74
ernie.cheng <ernie.cheng@perficient.com>
2017-10-13 13:54:59 +0800

    update locationService file

--- MeetingScheduleAR/Service/LocationService.swift
+++ MeetingScheduleAR/Service/LocationService.swift
@@ -56,9 +56,31 @@ class LocationService: NSObject, CLLocationManagerDelegate {
         locationManager.stopUpdatingHeading()
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        if newHeading.headingAccuracy < 0 { return }
+        let heading = newHeading.trueHeading > 0 ? newHeading.trueHeading : newHeading.magneticHeading
+        userHeading = heading
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "myNotificationName") , object: self, userInfo: nil)
+    }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            delegate?.trackingLocation(for: location)
+        }
+        currentLocation = manager.location
+    }
     
+    func updateLocation(currentLocation: CLLocation) {
+        guard let delegate = delegate else { return }
+        delegate.trackingLocation(for: currentLocation)
+    }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        updateLocationDidFailWithError(error: error)
+    }
     
-    
+    func updateLocationDidFailWithError(error: Error) {
+        guard let delegate = delegate else { return }
+        delegate.trackingLocationDidFail(with: error)
+    }
 }
